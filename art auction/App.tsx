3cdd975fbff6d2d4d540599f033e79de213db074
AlinaMautova <123927429+AlinaMautova@users.noreply.github.com>
2024-04-20 18:50:13 +0500

    Add files via upload

---
+++ art auction/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Solana Auction App</h1>
+      </header>
+      {/* Other components will be added here */}
+    </div>
+  );
+}
+
+export default App;
